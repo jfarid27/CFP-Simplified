024e1f7fea700bfd354bf727f2f877f5af902e5f
J. Farid <jrab227@gmail.com>
2016-02-04 20:11:54 -0500

    Moving network code

--- hask-simulation/Simulation.hs
+++ hask-simulation/Simulation.hs
@@ -1,17 +1,4 @@
 module Simulation (
 ) where
 
-import qualified Data.Map as Mapper
-
-type Id = Int
-newtype Node a = Node { getNode :: a }
-newtype Nodes a = Nodes { getNodes :: Mapper.Map Id (Node a) }
-newtype Edges = Edges { getEdges :: Mapper.Map Id [Id] }
-
-newtype Network a = Network { getNetwork :: (Nodes a, Edges) }
-
-nearestNeighbors :: Network a -> Id -> [Id]
-nearestNeighbors = undefined
-
-updateNode :: Network a -> Id -> Node a -> Network a
-updateNode = undefined
+import qualified Network as Network 
