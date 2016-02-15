278376760f70b397e90cdb0d9614261193d66b20
J. Farid <jrab227@gmail.com>
2016-02-15 17:35:26 -0500

    Added system wrapper for Wolff Ising program

--- simulation/Contagion/test_Contagion.py
+++ simulation/Contagion/test_Contagion.py
@@ -3,11 +3,14 @@ import simulation.Networks as Networks
 import pytest
 
 
-mockNodes = {1: {"spin": 1}, 2:{"spin": -1}, 3:{"spin": 0}, 4: {"spin": -1}}
-mockNodes2 = {1: {"spin": 1}, 2:{"spin": 1}, 3:{"spin": -1}, 4: {"spin": -1}, 5: {"spin": 1}}
-mockEdges = {1: [2], 2:[1,3], 3:[2, 4], 4:[3, 5], 5: [4]}
+mockNodes = {0: {"spin": 1}, 1:{"spin": -1}, 2:{"spin": 0}, 3: {"spin": -1}}
+mockNodes2 = {0: {"spin": 1}, 1:{"spin": 1}, 2:{"spin": -1}, 3: {"spin": -1}, 4: {"spin": 1}}
+mockEdges = {0: [1], 1:[0,2], 2:[1, 3], 3:[2, 4], 4: [3]}
 mockNetwork = Networks.Network(mockNodes, mockEdges)
 mockNetwork2 = Networks.Network(mockNodes2, mockEdges)
+
+def mockRandomNumGen():
+    return 1
 
 def canBeInfected(conditions, node):
     return True
@@ -24,19 +27,19 @@ def mockContagionSystem():
 
 @pytest.fixture
 def mockWolffSystem():
-    return Contagion.WolffIsing(mockNetwork2)
+    return Contagion.WolffIsing(mockNetwork2, 1, mockRandomNumGen)
 
 def test_spread(mockContagionSystem):
     mockContagionSystem.spread({}, [], {}, 1)
+    assert mockNodes[0]["spin"] == 1
     assert mockNodes[1]["spin"] == 1
-    assert mockNodes[2]["spin"] == 1
-    assert mockNodes[3]["spin"] == 0
-    assert mockNodes[4]["spin"] == -1
+    assert mockNodes[2]["spin"] == 0
+    assert mockNodes[3]["spin"] == -1
 
 def test_wolff_spread(mockWolffSystem):
     mockWolffSystem.aggrSpread({}, [], {'spin': -1}, 1)
+    assert mockNodes2[0]["spin"] == -1
     assert mockNodes2[1]["spin"] == -1
     assert mockNodes2[2]["spin"] == -1
     assert mockNodes2[3]["spin"] == -1
-    assert mockNodes2[4]["spin"] == -1
-    assert mockNodes2[5]["spin"] == 1
+    assert mockNodes2[4]["spin"] == 1
