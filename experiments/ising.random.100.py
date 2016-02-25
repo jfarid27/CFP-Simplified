d08f8b989ee3d3742dcb5445473e3fc74f21276a
J. Farid <jrab227@gmail.com>
2016-02-25 00:30:05 -0500

    Added a better experiment

--- experiments/ising.random.100.py
+++ experiments/ising.random.100.py
@@ -11,15 +11,15 @@ import simulation.MCMC as MCMC
 random.seed(int("54e22d", 16))
 
 steps = 1000000
-side = 32
+side = 100
 spins = side * side
 
 def observablesWrap(openedFile):
     def wrap(system, observations):
         observables(system, observations)
-        data = str.format("{},{},{},{}", observations['lastAbsEnergy'], \
-            observations['lastSqAbsEnergy'], observations['lastAbsMag'],\
-            observations['lastSqAbsMag'])
+        data = str.format("{},{},{},{}", observations['lastEnergy'], \
+            observations['lastSqEnergy'], observations['lastMag'],\
+            observations['lastSqMag'])
         openedFile.write(data + '\n')
     return wrap
 
@@ -30,42 +30,40 @@ def observables(system, observations):
     for k in range(system.wolffIsing.network.numNodes):
         M += S[k]['spin']
         E -=  S[k]['spin'] * sum([S[neighbor]['spin'] for neighbor in system.wolffIsing.network.edges[k]])
-    observations['absSumEnergy'] += abs(E)
-    observations['absSumSqEnergy'] += abs(E) ** 2
-    observations['absSumMag'] += abs(M)
-    observations['absSumSqMag'] += abs(M) ** 2
-    observations['lastAbsEnergy'] = abs(E)
-    observations['lastSqAbsEnergy'] = abs(E) ** 2
-    observations['lastAbsMag'] = abs(M)
-    observations['lastSqAbsMag'] = abs(M) ** 2
+    observations['lastEnergy'] = E
+    observations['lastSqEnergy'] = E ** 2
+    observations['lastMag'] = M
+    observations['lastSqMag'] = M ** 2
     
 def randomSpin(x, y):
     return {'spin': random.choice([-1, 1])}
 
-def experiment(temperature):
+def experimentSingleNetwork(network):
+    def closure(temperature):
+        return experiment(temperature, network)
+    return closure
+
+def experiment(temperature, network):
     print("starting temperature", temperature)
-    filename = 'data/ising_lattice_energy_mag_' + \
+    filename = 'data/ising.random.100.' + \
         str.format("{}", temperature) + ".csv"
     openedFile = open(filename, 'w')
-    network = Network.Lattice2DNP(side, side, {}, {})
-    network.build(randomSpin)
     contagion = Contagion.WolffIsing(network, 1/float(temperature), random.random)
     system = System.WolffIsing(contagion, random.randint)
     montecarlo = MCMC.MCMC(system, random.random)
     observations = {}
-    observations['absSumEnergy'] = 0
-    observations['absSumSqEnergy'] = 0
-    observations['absSumMag'] = 0
-    observations['absSumSqMag'] = 0
-    observations['lastAbsEnergy'] = 0
-    observations['lastAbsMag'] = 0
-    observations['lastSqAbsEnergy'] = 0
-    observations['lastSqAbsMag'] = 0
+    observations['lastEnergy'] = 0
+    observations['lastMag'] = 0
+    observations['lastSqEnergy'] = 0
+    observations['lastSqMag'] = 0
     montecarlo.simulate(observablesWrap(openedFile), observations, steps)
     openedFile.close()
     return observations
 
 if __name__ == '__main__':
+    network = Network.Lattice2DNP(side, side, {}, {})
+    network.build(randomSpin)
     temperatures = [float(x)/10 for x in range(1, 50)]
-    p = Pool(len(temperatures))
-    p.map(experiment, temperatures)
+    temperatures.reverse()
+    experiments = map(experimentSingleNetwork(network), temperatures)
+    results = [result for result in experiments]
